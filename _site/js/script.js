daf6d33b2a8927a05eca11c4818e2fc8f7e77a4b
LeneS99 <31234291+LeneS99@users.noreply.github.com>
2017-09-06 17:29:24 +0200

    sticky nav homepage

--- _site/js/script.js
+++ _site/js/script.js
@@ -4,3 +4,17 @@
      $(".burger-button").toggleClass("active");
    });
 });
+
+// Stiky nav on homepage
+
+var targetPos = $('.nav-bar a').offset().top;
+
+$(window).scroll(function() {
+  var scrollPos = $(this).scrollTop();
+
+if ( scrollPos > targetPos ) {
+  $('#home-navigation').addClass('fixed-nav');
+} else {
+  $('#home-navigation').removeClass('fixed-nav');
+}
+});
